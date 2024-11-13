f0cc3969846edd1c1bbaf6c58c49f9d0edb00022
brian.musili <brian.musili@systechafrica.com>
2024-11-13 18:34:23 +0300

    sidebra

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import styled from 'styled-components'
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -8,11 +10,18 @@ const StyledSidebar = styled.aside`
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/-1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 const Sidebar = () => {
   return (
-    <StyledSidebar>Sidebar</StyledSidebar>
+    <StyledSidebar>
+      <Logo/>
+      <MainNav/>
+    </StyledSidebar>
   )
 }
 
