e177abca9e50b0c5834cff42832f16cb655292cd
brian.musili <brian.musili@systechafrica.com>
2024-12-02 17:36:45 +0300

    Create modal with react portal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin();
 
   const { isEditing, editCabin } = useEditCabin();
@@ -33,14 +33,20 @@ function CreateCabinForm({ cabinToEdit = {} }) {
       editCabin(
         { newCabinData: { ...data, image }, id: editId },
         {
-          onSuccess: () => reset(),
+          onSuccess: () => {
+            () => onCloseModal?.();
+            reset();
+          },
         }
       );
     } else {
       createCabin(
         { ...data, image },
         {
-          onSuccess: () => reset(),
+          onSuccess: () => {
+            reset;
+            () => onCloseModal?.();
+          },
         }
       );
     }
@@ -51,7 +57,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)} type={onCloseModal ? 'modal' : 'regular'}>
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -132,7 +138,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset" disabled={isWorking}>
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button> {isEditSession ? "Update Cabin" : " Create new cabin"}</Button>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,10 @@
-/* eslint-disable no-unused-vars */
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
-import { useState } from "react";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-
-  const [showForm, setShowForm] = useState(false);
-
 
   return (
     <>
@@ -21,10 +15,7 @@ function Cabins() {
 
     <Row>
       <CabinTable/>
-      <Button onClick={() => setShowForm(show => !show)}>Add new Cabin</Button>
-      {
-        showForm && <CreateCabinForm/>
-      }
+    <AddCabin/>
     </Row>
     </>
   );
