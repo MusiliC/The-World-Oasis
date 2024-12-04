f1d46735bb900838bbc829c46e2e8f15aadb2f16
brian.musili <brian.musili@systechafrica.com>
2024-12-04 07:35:16 +0300

    Enable filter

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +35,30 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currFilter = searchParams.get(filterField) || options[0].value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option, i) => (
+        <FilterButton
+          key={i}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
