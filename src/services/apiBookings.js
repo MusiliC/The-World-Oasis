094f999988f6f0c37de243fa0d1783b38be8a8bf
brian.musili <brian.musili@systechafrica.com>
2024-12-05 07:10:44 +0300

    sample data upload fix

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -51,7 +51,7 @@ export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
     // .select('*')
-    .select("*, guests(fullName)")
+    .select("*, guests(full_name)")
     .gte("startDate", date)
     .lte("startDate", getToday());
 
@@ -67,7 +67,7 @@ export async function getStaysAfterDate(date) {
 export async function getStaysTodayActivity() {
   const { data, error } = await supabase
     .from("bookings")
-    .select("*, guests(fullName, nationality, countryFlag)")
+    .select("*, guests(full_name, nationality, countryFlag)")
     .or(
       `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
     )
