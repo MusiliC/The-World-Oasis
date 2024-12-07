f8b3281e79d8778a74a107d00d2cf45bcf1c8eec
brian.musili <brian.musili@systechafrica.com>
2024-12-07 16:29:38 +0300

    Implement pagination

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import useGetBookings from "./useGetBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { isPending, bookings } = useGetBookings();
@@ -31,6 +32,10 @@ function BookingTable() {
           )}
         />
       </Table>
+
+      <Table.Footer>
+        <Pagination count={15} />
+      </Table.Footer>
     </Menus>
   );
 }
