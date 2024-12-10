b6f7b12e48cd8974adc68ff6d9418dbed08faa1e
brian.musili <brian.musili@systechafrica.com>
2024-12-10 21:43:35 +0300

    Protected routing

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin(){
 
+    const queryClient = useQueryClient()
+
     const navigate = useNavigate();
 
    const {mutate: login, isPending: isLoginLoading} =  useMutation({
         mutationFn:({email,password}) => loginApi({email, password}),
-        onSuccess: () => {
+        onSuccess: (data) => {
+            queryClient.setQueriesData(['user'], data)
             navigate("/dashboard")
         },
 
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,12 +1,10 @@
 import supabase from "./supabase";
 
-export async function login({email, password}) {
-    
-let { data, error } = await supabase.auth.signInWithPassword({
-  email,
-  password
-})
-
+export async function login({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
   if (error) {
     console.error(error);
@@ -14,8 +12,23 @@ let { data, error } = await supabase.auth.signInWithPassword({
   }
 
   console.log(data);
-  
 
   return data;
+}
 
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  let { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+
+  console.log(data);
+
+  return data?.user;
 }
